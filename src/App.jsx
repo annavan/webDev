296a05e489d3253049ca3df91888615fecaaf23a
annavan <8annavan@gmail.com>
2025-03-06 12:18:36 -0500

    feature 3 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import { useState } from 'react'
-import Components from "./Components/Components.js";
+import Components from "./Components/Components.jsx";
 import './App.css'
 //import * as Env from './environments.js'
 import Parse from 'parse'
 
-Env = {
+const Env = {
   APPLICATION_ID: "mHEinGaD7EJLQNC9sXnY5fqHVPys1SAgPH428T3j",
   JAVASCRIPT_KEY: "ZjOrgNXKZOQkMsyD8KVtTdXWWh76lwpxil33xeNX",
   SERVER_URL: "https://parseapi.back4app.com"
 };
 
+Parse.initialize(Env.APPLICATION_ID, Env.JAVASCRIPT_KEY);
+Parse.serverURL = Env.SERVER_URL;
+
 function App() {
 
   return (
     <>
-    {/* print text in black */}
-    <h1>App</h1>
-    <Components />
-
+      <h1>App</h1>
+      <Components />
     </>
   )
 }
--- src/Components/Components.jsx
+++ src/Components/Components.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-// import Main from "./Main/Main.jsx";
+import Main from "./Main/Main.jsx";
 
 const Components = () => {
     return (
         <div>
             {/* print for debug */}
             <h1>Components</h1>
-            {/* <Main /> */}
+            <Main />
         </div>
     );
 };
